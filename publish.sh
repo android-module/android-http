62412f4f424e5da0a6d9778af951d279705b46db
Caldremch <caldremch@163.com>
2022-08-16 01:06:56 +0800

    feat:
    1. add full future task for bind something
    
    2.set publish script

---
+++ publish.sh
@@ -0,0 +1,3 @@
+#./gradlew -PremoteDep=true android-http:publishAllPublicationsToMavenRepository
+#./gradlew -PremoteDep=true android-http-retrofit-rxjava3-impl:publishAllPublicationsToMavenRepository
+./gradlew -PremoteDep=true android-http-viewmodel:publishAllPublicationsToMavenRepository
