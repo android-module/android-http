ddc72e71d93a4f81fbe48a6e234d24b5e176082e
Caldremch <caldremch@163.com>
2022-10-08 10:49:52 +0800

    feat:
    update version

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-#./gradlew -PremoteDep=true android-http:publishAllPublicationsToMavenRepository
+./gradlew -PremoteDep=true http-core:publishAllPublicationsToMavenRepository
 #./gradlew -PremoteDep=true android-http-retrofit-rxjava3-impl:publishAllPublicationsToMavenRepository
-./gradlew -PremoteDep=true android-http-retrofit-rxjava3-impl:publishToMavenLocal
-#./gradlew -PremoteDep=true android-http-viewmodel:publishAllPublicationsToMavenRepository
+#./gradlew -PremoteDep=true android-http-retrofit-rxjava3-impl:publishToMavenLocal
+#./gradlew -PremoteDep=true coroutine-http:publishAllPublicationsToMavenRepository
